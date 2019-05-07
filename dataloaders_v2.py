fix: FID and dataloader for distributed training.... Gradient Penalty for Distributed NOT WORKING currenlty

dataloaders_v2.py
@@ -4,6 +4,7 @@ import utils
 import glob
 import numpy as np
 from torchvision import transforms
+from data_sampelrs import ValidationSampler, TrainSampler
 
 
 class DeepPrivacyDataset(torch.utils.data.Dataset):
@@ -91,7 +92,7 @@ def load_dataset(dirpath, imsize, batch_size, distributed):
     assert os.path.isfile(
         landmark_filepath), "Did not find the landmark data. Looked in: {}".format(landmark_filepath)
     landmarks = torch.load(landmark_filepath).float() / imsize
-    validation_size = int(0.02*len(images))
+    validation_size = 10000#int(0.02*len(images))
     images_train, images_val = images[:-validation_size], images[-validation_size:]
     bbox_train, bbox_val = bounding_boxes[:-validation_size], bounding_boxes[-validation_size:]
     lm_train, lm_val = landmarks[:-validation_size], landmarks[-validation_size:]
@@ -99,15 +100,13 @@ def load_dataset(dirpath, imsize, batch_size, distributed):
     dataset_train = DeepPrivacyDataset(images_train, bbox_train, lm_train, True)
     dataset_val = DeepPrivacyDataset(images_val, bbox_val, lm_val, False)
     print("LEN DATASET VAL:", len(dataset_val), len(images_val))
-
-    train_sampler, val_sampler = None, None
-    if distributed:
-        train_sampler = torch.utils.data.DistributedSampler(dataset_train)
-        val_sampler = torch.utils.data.DistributedSampler(dataset_val)
+   
+    train_sampler = TrainSampler(dataset_train)
+    val_sampler = ValidationSampler(dataset_val)
     
     dataloader_train = torch.utils.data.DataLoader(dataset_train,
                                                    batch_size=batch_size,
-                                                   shuffle=(train_sampler is None),
+                                                   shuffle=train_sampler is None,
                                                    sampler=train_sampler,
                                                    num_workers=8,
                                                    drop_last=True,

metrics/fid.py
@@ -42,7 +42,7 @@ from scipy.misc import imread
 from scipy import linalg
 from torch.autograd import Variable
 from torch.nn.functional import adaptive_avg_pool2d
-from utils import to_cuda
+from utils import to_cuda, is_distributed, gather_tensor
 from metrics.inception import InceptionV3
 dims=2048
 block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
@@ -81,7 +81,10 @@ def get_activations(images, model, batch_size=64):
 
     n_batches = d0 // batch_size
     n_used_imgs = n_batches * batch_size
-    pred_arr = np.zeros((n_used_imgs, dims))
+    if is_distributed():
+        pred_arr = np.zeros((n_used_imgs*torch.distributed.get_world_size(), dims))
+    else:
+        pred_arr = np.zeros((n_used_imgs, dims))
 
     for i in range(n_batches):
 
@@ -97,9 +100,13 @@ def get_activations(images, model, batch_size=64):
         # This happens if you choose a dimensionality not equal 2048.
         if pred.shape[2] != 1 or pred.shape[3] != 1:
             pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
-
-        pred_arr[start:end] = pred.cpu().data.numpy().reshape(batch_size, -1)
-
+        if is_distributed():
+            pred = gather_tensor(pred).reshape(batch_size*torch.distributed.get_world_size(), -1)
+            start = i * batch_size * torch.distributed.get_world_size()
+            end = start + batch_size * torch.distributed.get_world_size()
+        else:
+            pred = pred.reshape(batch_size, -1)
+        pred_arr[start:end] = pred.cpu().data.numpy()
     return pred_arr
 
 
@@ -192,7 +199,7 @@ def calculate_fid(images1, images2, batch_size):
     assert images2.max() <= 1
     assert images2.max() >= 0
     model = get_model()
-    #model = to_cuda(model)
+    model = to_cuda(model)
     m1, s1 = calculate_activation_statistics(images1, model, batch_size)
     m2, s2 = calculate_activation_statistics(images2, model, batch_size)
     del model

start_train.py
@@ -15,12 +15,13 @@ model_index = sys.argv.index("--model")
 model_name = sys.argv[model_index+1]
 docker_container = "haakohu_{}".format(model_name)
 print("docker container name:", docker_container)
+os.system("docker rm {}".format(docker_container))
 
 distributed_command = "" if num_gpus <= 1 else "-m torch.distributed.launch --nproc_per_node {}".format(num_gpus)
 command = "nvidia-docker run --name {} \
         -v /dev/log:/home/haakohu/DeepPrivacy/log -u 1174424 -v {}:/workspace -v /raid/userdata/haakohu/deep_privacy/data:/workspace/data \
            -e CUDA_VISIBLE_DEVICES={}  --log-opt max-size=50m\
-           haakohu/pytorch_beta python {} train.py {}".format(
+           haakohu/pytorch python {} train.py {}".format(
             docker_container,
             filedir,
             gpu_id,

train.py
@@ -7,7 +7,7 @@ import torch
 from apex import amp
 import torchvision
 import utils
-from utils import load_checkpoint, save_checkpoint, to_cuda
+from utils import load_checkpoint, save_checkpoint, to_cuda, reduce_tensor, amp_state_has_overflow, is_distributed, gather_tensor
 from unet_model import Generator, Discriminator, DeepDiscriminator
 from dataloaders_v2 import load_celeba_condition, load_ffhq_condition, load_yfcc100m, load_yfcc100m128
 from options import load_options, print_options, DEFAULT_IMSIZE
@@ -15,7 +15,15 @@ from metrics import fid
 import tqdm
 import apex
 
-torch.backends.cudnn.benchmark = True
+if False:
+    torch.manual_seed(0)
+    np.random.seed(0)
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.set_printoptions(precision=10)
+else:
+
+    torch.backends.cudnn.benchmark = True
 
 
 def isnan(x):
@@ -209,7 +217,7 @@ class DataPrefetcher():
         next_condition = self.next_condition
         next_landmark = self.next_landmark
         self.preload(transition_variable)
-        return next_image, next_condition, next_landmark
+        return next_image, next_condition, next_landmark#[:, :1]
 
 
 def interpolate_image(pool, images, transition_variable):
@@ -270,9 +278,11 @@ class Trainer:
         ]
         self.start_time = time.time()
         if self.local_rank == 0:
-            self.writer = tensorboardX.SummaryWriter(options.summaries_dir)
+            self.writer = tensorboardX.SummaryWriter(
+                os.path.join(options.summaries_dir, "train")
+            )
             self.validation_writer = tensorboardX.SummaryWriter(
-                os.path.join(options.summaries_dir, "validation"))
+                os.path.join(options.summaries_dir, "val"))
         if not self.load_checkpoint():
             self.discriminator, self.generator = init_model(options.imsize,
                                                             self.pose_size,
@@ -432,7 +442,7 @@ class Trainer:
         self.current_imsize *= 2
 
         self.batch_size = self.batch_size_schedule[self.current_imsize] // self.world_size
-        self.log_variable("stats/batch_size", self.batch_size)
+        
         self.transition_step += 1
 
     def update_running_average_generator(self):
@@ -554,13 +564,12 @@ class Trainer:
         self.discriminator.train()
         del data_prefetcher
 
-    def check_loss_scale(self):
-        return
-        for optimizer in [self.d_optimizer, self.g_optimizer]:
-            if optimizer.loss_scaler._loss_scale == 0:
-                optimizer.loss_scaler._loss_scale = 2**15
-                print("Loss scale was too small. Scaled back to 1. Step:", self.prev_step, "Current step:", self.global_step)
-                self.prev_step = self.global_step
+    def log_loss_scales(self):
+        if self.local_rank != 0:
+            return
+        for loss_idx, loss_scaler in enumerate(amp._amp_state.loss_scalers):
+            self.log_variable("amp/loss_scale_{}".format(loss_idx), loss_scaler._loss_scale)
+        
 
     def train(self):
         for epoch in range(self.start_epoch, int(1e16)):
@@ -568,7 +577,6 @@ class Trainer:
             for i in range(len(self.dataloader_train)):
                 batch_start_time = time.time()
                 self.generator.train()
-                self.check_loss_scale()
                 if self.is_transitioning:
                     self.transition_variable = (
                         (self.global_step-1) % self.transition_iters) / self.transition_iters
@@ -578,8 +586,8 @@ class Trainer:
                         self.transition_variable)
                     self.running_average_generator.update_transition_value(
                         self.transition_variable)
+                
                 real_data, condition, landmarks = prefetcher.next(self.transition_variable)
-                torchvision.utils.save_image(denormalize_img(real_data), "test.jpg", nrow=10)
                 # Forward G
                 fake_data = self.generator(condition, landmarks)
                 # Train Discriminator
@@ -650,8 +658,8 @@ class Trainer:
                         fake_scores = reduce_tensor(fake_scores.data, self.world_size)
                         epsilon_penalty = reduce_tensor(epsilon_penalty.data, self.world_size)
                     torch.cuda.synchronize()
-                    if self.local_rank == 0:
-                        
+                    self.log_loss_scales()
+                    if self.local_rank == 0 and not amp_state_has_overflow():
                         self.log_variable(
                             'discriminator/wasserstein-distance',
                             wasserstein_distance.mean().item())
@@ -669,6 +677,7 @@ class Trainer:
                         self.log_variable("stats/nsec_per_img", nsec_per_img)
                         self.log_variable(
                             "stats/training_time_minutes", self.total_time)
+                    
                         
                 self.global_step += self.batch_size*self.world_size
                 
@@ -727,19 +736,13 @@ class Trainer:
                         # Save image after transition
                         self.save_transition_image(False)
                         self.save_checkpoint(epoch)
+                        self.log_variable("stats/batch_size", self.batch_size)
+                        self.log_variable("stats/learning_rate", self.learning_rate)
 
                         break
             self.save_checkpoint(epoch)
 
-
-def reduce_tensor(tensor, world_size):
-    rt = tensor.clone()
-    torch.distributed.all_reduce(rt, op=torch.distributed.ReduceOp.SUM)
-    rt /= world_size
-    return rt
-
 if __name__ == '__main__':
     options = load_options()
-
     trainer = Trainer(options)
     trainer.train()

utils.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import torch
-
+from apex.amp import _amp_state
 
 def to_cuda(elements):
     if torch.cuda.is_available():
@@ -98,7 +98,7 @@ def _rampdown_linear(epoch, num_epochs, rampdown_length):
 
 def get_transition_value(x_old, x_new, transition_variable):
     assert x_old.shape == x_new.shape
-    return (1-transition_variable) * x_old + transition_variable*x_new
+    return torch.lerp(x_old, x_new, transition_variable)
 
 
 def init_model(start_channel_size, num_levels, model):
@@ -119,3 +119,29 @@ def init_model(start_channel_size, num_levels, model):
 def truncated_normal(mean, std, size):
     normal = torch.zeros(size).normal_(mean=mean, std=std)
     return normal
+
+
+
+def gather_tensor(tensor):
+    to_return = [torch.zeros_like(tensor, device=tensor.device) for i in range(torch.distributed.get_world_size())]
+    torch.distributed.all_gather_multigpu(to_return, tensor)
+    return torch.cat(to_return, dim=0)
+
+def reduce_tensor(tensor, world_size=None):
+    rt = tensor.clone()
+    torch.distributed.all_reduce(rt, op=torch.distributed.ReduceOp.SUM)
+    rt /= world_size
+    return rt
+
+
+def is_distributed():
+    a = torch.distributed.is_available()
+    b = torch.distributed.is_initialized()
+    return a and b and torch.distributed.get_world_size() > 1
+
+
+def amp_state_has_overflow():
+    for loss_scaler in _amp_state.loss_scalers:
+        if loss_scaler._has_overflow:
+            return True
+    return False