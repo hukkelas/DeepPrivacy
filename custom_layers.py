fix: use DataParallel instead of distributed training (fix gradient penalty bug for NUM_GPU>1))

custom_layers.py
@@ -14,8 +14,8 @@ class WSConv2d(nn.Module):
         self.conv = nn.Conv2d(in_channels=inCh, out_channels=outCh, kernel_size=kernelSize, stride=1, padding=padding)
 
         # new bias to use after wscale
-        self.bias = self.conv.bias
-        self.bias = nn.Parameter(self.bias.view(1, self.bias.shape[0], 1, 1))
+        bias = self.conv.bias
+        self.bias = nn.Parameter(bias.view(1, bias.shape[0], 1, 1))
         self.conv.bias = None
         
         # calc wt scale

options.py
@@ -46,15 +46,7 @@ def print_options(dic):
 
 def check_distributed(options):
     options.world_size = 1
-    if "WORLD_SIZE" in os.environ:
-        options.distributed = int(os.environ["WORLD_SIZE"]) > 1
-    else:
-        options.distributed = False
-    if options.distributed:
-        print("Enabling distributed training. Number of GPUs:", os.environ["WORLD_SIZE"])
-        torch.cuda.set_device(options.local_rank)
-        torch.distributed.init_process_group(backend="nccl", init_method="env://")
-        options.world_size = torch.distributed.get_world_size()
+    options.distributed = False
 
 
 def load_options():

train.py
@@ -14,6 +14,7 @@ from options import load_options, print_options, DEFAULT_IMSIZE
 from metrics import fid
 import tqdm
 import apex
+from data_tools.data_utils import DataPrefetcher
 
 if False:
     torch.manual_seed(0)
@@ -22,7 +23,6 @@ if False:
     torch.backends.cudnn.deterministic = True
     torch.set_printoptions(precision=10)
 else:
-
     torch.backends.cudnn.benchmark = True
 
 
@@ -72,16 +72,11 @@ class NetworkWrapper(torch.nn.Module):
         super().__init__()
         self.network = network
         self.distributed = distributed
-        if distributed:
-            # Does not work with delay_allreduce=False 
-            # 
-            self.forward_block = torch.nn.parallel.DistributedDataParallel(
-                self.network,
-                device_ids=[local_rank],
-                output_device=local_rank
-            )
-        else:
-            self.forward_block = self.network
+        #self.forward_block = torch.nn.DataParallel(
+        #    self.network,
+        #    device_ids=list(range(torch.cuda.device_count() ))
+        #)
+        self.forward_block = to_cuda(self.network)
 
     def forward(self, *inputs):
         return self.forward_block(*inputs)
@@ -107,7 +102,11 @@ class NetworkWrapper(torch.nn.Module):
         network.load_state_dict(self.network.state_dict())
         network.extend(channel_size)
         del self.network
-        self.network = to_cuda(network)
+        self.network = network
+        self.forward_block = torch.nn.DataParallel(
+            self.network,
+            device_ids=list(range(torch.cuda.device_count()))
+        )
         
 
     def update_transition_value(self, value):
@@ -135,15 +134,15 @@ def init_model(imsize, pose_size, start_channel_dim, image_channels, distributed
                                   start_channel_dim,
                                   pose_size)
     generator = Generator(pose_size, start_channel_dim, image_channels)
-    to_cuda([discriminator, generator])
-    #discriminator, generator = wrap_models([discriminator, generator], distributed)
+    #to_cuda([discriminator, generator])
+    discriminator, generator = wrap_models([discriminator, generator], distributed, 0)
     return discriminator, generator
 
 
 def wrap_models(models, distributed, local_rank):
     if isinstance(models, tuple) or isinstance(models, list):
-        return [to_cuda(NetworkWrapper(x, distributed, local_rank)) for x in models]
-    return to_cuda(NetworkWrapper(models, distributed, local_rank))
+        return [NetworkWrapper(x, distributed, local_rank) for x in models]
+    return NetworkWrapper(models, distributed, local_rank)
 
 
 def save_images(writer, images, global_step, directory):
@@ -165,58 +164,6 @@ def preprocess_images(image, transition_variable, pool=torch.nn.AvgPool2d(2, 2))
     image = image * 2 - 1
     image = interpolate_image(pool, image, transition_variable)
     return image
-
-
-class DataPrefetcher():
-
-    def __init__(self, loader, transition_variable, pose_size):
-        self.pool = torch.nn.AvgPool2d(2, 2)
-        self.loader = iter(loader)
-        self.stream = torch.cuda.Stream()
-        self.preload(transition_variable)
-        self.pose_size = pose_size
-
-    def preload(self, transition_variable):
-        try:
-            self.next_image, self.next_condition, self.next_landmark = next(self.loader)
-        except StopIteration:
-            self.next_image = None
-            self.next_condition = None
-            self.next_landmark = None
-            return
-        with torch.cuda.stream(self.stream):
-            self.next_image = self.next_image.cuda(non_blocking=True).float()
-            self.next_condition = self.next_condition.cuda(non_blocking=True).float()
-            self.next_landmark = self.next_landmark.cuda(non_blocking=True)
-            
-            self.next_image = self.next_image / 255
-            self.next_image = self.next_image*2 - 1
-
-            self.next_condition = self.next_condition / 255
-            self.next_condition = self.next_condition*2 - 1
-
-            self.next_image = interpolate_image(self.pool,
-                                                self.next_image,
-                                                transition_variable)
-            self.next_condition = interpolate_image(self.pool,
-                                                    self.next_condition,
-                                                    transition_variable)
-
-    def next(self, transition_variable):
-        torch.cuda.current_stream().wait_stream(self.stream)
-        next_image = self.next_image
-        next_condition = self.next_condition
-        next_landmark = self.next_landmark
-        self.preload(transition_variable)
-        return next_image, next_condition, next_landmark[:, :self.pose_size]
-
-
-def interpolate_image(pool, images, transition_variable):
-    y = pool(images)
-    y = torch.nn.functional.interpolate(y, scale_factor=2)
-
-    images = utils.get_transition_value(y, images, transition_variable)
-    return images
 
 
 class Trainer:
@@ -421,16 +368,16 @@ class Trainer:
         self.running_average_generator = amp.initialize(self.running_average_generator, None, opt_level=self.opt_level)
         self.running_average_generator = NetworkWrapper(
             self.running_average_generator, self.distributed, self.local_rank)
-        self.running_average_generator = to_cuda(
-            self.running_average_generator)
 
     def extend_running_average_generator(self, current_channels):
         g = self.running_average_generator
         g.extend(current_channels)
+        
         for avg_param, cur_param in zip(g.new_parameters(), self.generator.new_parameters()):
             assert avg_param.data.shape == cur_param.data.shape, "AVG param: {}, cur_param: {}".format(avg_param.shape, cur_param.shape)
             avg_param.data = cur_param.data
         self.running_average_generator = self.running_average_generator.network
+        self.running_average_generator.cuda()
         self.running_average_generator = amp.initialize(self.running_average_generator, None, opt_level=self.opt_level)
         self.running_average_generator = NetworkWrapper(self.running_average_generator, self.distributed, self.local_rank)
 
@@ -463,16 +410,21 @@ class Trainer:
         self.initialize_amp()
 
     def initialize_amp(self):
-        if isinstance(self.generator, NetworkWrapper):
-            self.generator = self.generator.network
-        if isinstance(self.discriminator, NetworkWrapper):
-            self.discriminator = self.discriminator.network
+        
+        if False:#isinstance(self.generator, NetworkWrapper):
+            self.generator = to_cuda(self.generator.network)
+        else:
+            to_cuda(self.generator)
+        if False:#isinstance(self.discriminator, NetworkWrapper):
+            self.discriminator = to_cuda(self.discriminator.network)
+        else:
+            to_cuda(self.discriminator)
         [self.generator, self.discriminator], [self.g_optimizer, self.d_optimizer] = amp.initialize(
             [self.generator, self.discriminator],
             [self.g_optimizer, self.d_optimizer],
             opt_level=self.opt_level,
             num_losses=4)
-        self.discriminator, self.generator = wrap_models([self.discriminator, self.generator], self.distributed, self.local_rank)
+        #self.discriminator, self.generator = wrap_models([self.discriminator, self.generator], self.distributed, self.local_rank)
 
     def log_variable(self, name, value, log_to_validation=False):
         if self.local_rank == 0:
@@ -586,14 +538,7 @@ class Trainer:
         gradient_pen = gradient_penalty(
             real_data.data, fake_data.detach(), self.discriminator,
             condition, landmarks, self.wgan_gp_scaler)
-        if self.distributed:
-            should_skip = torch.tensor(gradient_pen is None, device=fake_data.device)
-            torch.distributed.all_reduce(should_skip,
-                                         op=torch.distributed.ReduceOp.SUM)
-            should_skip = should_skip.item() != 0
-        else:
-            should_skip = gradient_pen is None
-        if should_skip:
+        if gradient_pen is None:
             return None
         # Epsilon penalty
         epsilon_penalty = (real_scores ** 2).squeeze()
@@ -671,7 +616,7 @@ class Trainer:
                     fpath = os.path.join(dirname, "step_{}.ckpt".format(self.global_step))
                     self.save_checkpoint(self.global_step, fpath)
                 if i % 50 == 0:
-                    if self.distributed:
+                    if False:#self.distributed:
                         wasserstein_distance = reduce_tensor(wasserstein_distance.data,
                                                              self.world_size)
 

utils.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import torch
-from apex.amp import _amp_state
+from apex.amp._amp_state import _amp_state
 
 def to_cuda(elements):
     if torch.cuda.is_available():