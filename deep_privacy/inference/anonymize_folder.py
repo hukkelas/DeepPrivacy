56fb265ebfa193d1837a7c713621e3527d42a18b
Lea Vauchier <lea.vauchier@kitware.com>
2020-01-03 15:07:25 +0100

    Activate tight bbox anonymisation in video/folder inference

--- deep_privacy/inference/anonymize_folder.py
+++ deep_privacy/inference/anonymize_folder.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     generator, imsize, source_path, image_paths, save_path = infer.read_args()
 
     anonymizer = deep_privacy_anonymizer.DeepPrivacyAnonymizer(
-        generator, 128, use_static_z=True)
+        generator, 128, use_static_z=True, replace_tight_bbox=True)
 
     anonymizer.anonymize_folder(source_path, save_path)
